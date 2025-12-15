3d8fe7a5f041cad110a5d2db501634b6e19170fd
brave.yyp <mr.yeyintphyoo@gmail.com>
2025-12-15 22:43:56 +0400

    Update Canary-V2 Text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now Phase2-v2
+          Learn React Phase2-V2
         </a>
       </header>
     </div>
