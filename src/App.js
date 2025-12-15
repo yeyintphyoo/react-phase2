df193c0a0828da4405f03a65c584bd0d7f9eabcc
brave.yyp <mr.yeyintphyoo@gmail.com>
2025-12-15 21:54:36 +0400

    First Deploy on Phase2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now
+          Learn React Now Phase2-v1
         </a>
       </header>
     </div>
